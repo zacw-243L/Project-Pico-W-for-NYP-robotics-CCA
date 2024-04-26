a444e3c767e3696da1c9ef5ed4ed2aeb19ce1c54
Zachary <qwerty.amousy123442x@gmail.com>
2024-04-26 17:56:06 +0800

    removed LED blinker

--- Experimental Features/the original movement scheme in HTTP server/main.py
+++ Experimental Features/the original movement scheme in HTTP server/main.py
@@ -39,17 +39,6 @@ while timeout > 0:
     print('Waiting for connection...')
     time.sleep(1)
 
-
-# Define blinking function for onboard LED to indicate error codes
-def blink_onboard_led(num_blinks):
-    led = machine.Pin('LED', machine.Pin.OUT)
-    for i in range(num_blinks):
-        led.on()
-        time.sleep(.2)
-        led.off()
-        time.sleep(.2)
-
-
 # Handle connection error
 # Error meanings
 # 0  Link Down
